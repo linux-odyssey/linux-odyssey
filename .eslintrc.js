ad4dc6d5769e2a35c844ac16591db94d4f3d232f
wancat <wancat@wancat.cc>
2023-07-14 21:19:53 +0800

    Setup ESLint + Prettier, Re-setup Express

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+    'plugin:prettier/recommended', // We added this
+  ],
+  parserOptions: {
+    // parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto', semi: false }], // We added this
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
