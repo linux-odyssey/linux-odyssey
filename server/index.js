8901b385ad3f12491a42547a5f2aa568987cb0c2
wancat <wancat@wancat.cc>
2023-07-18 20:11:05 +0800

    Add swagger

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
+import fs from 'fs'
 import express from 'express'
 import http from 'http'
+import YAML from 'yaml'
+import swaggerUI from 'swagger-ui-express'
+
 import socketServer from './socket.js'
 
 const port = 3000
 const app = express()
 const server = http.createServer(app)
 socketServer(server)
+
+const file = fs.readFileSync('./api/swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
