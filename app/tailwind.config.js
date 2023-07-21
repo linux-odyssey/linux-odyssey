589b0736b9e9f3e673de5f3fe1b54af187d4c0bd
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-07-21 18:02:51 +0800

    merge Learning Page UI prototype into App.vue

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          primary: '#282A33',
+          secondary: '#131313',
+          DEFAULT: '#010103',
+        },
+        text: {
+          primary: '#39FF14',
+          secondary: '#ADADB5',
+          line: '#4A4B63',
+          DEFAULT: '#FFFFFF',
+        },
+        disabled: {
+          background: '#454552',
+          text: '#6A6B8C',
+        },
+        border: {
+          DEFAULT: '#526B4E',
+        },
+      },
+    },
   },
   plugins: [],
 }
