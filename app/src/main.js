06220e6b30ac6b3d5af2d5a6a09303c4f97e002e
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-07-31 12:02:12 +0800

    import fontawesome api

--- app/src/main.js
+++ app/src/main.js
@@ -2,4 +2,43 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {
+  faBarsStaggered,
+  faArrowLeft,
+  faList,
+  faGear,
+  faTriangleExclamation,
+  faExpand,
+  faTerminal,
+  faArrowRotateLeft,
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faLightbulb,
+  faCircleQuestion,
+  faCircleRight,
+} from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(
+  faBarsStaggered,
+  faArrowLeft,
+  faTriangleExclamation,
+  faGear,
+  faExpand,
+  faList,
+  faLightbulb,
+  faCircleQuestion,
+  faArrowRotateLeft,
+  faCircleRight,
+  faTerminal
+)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
