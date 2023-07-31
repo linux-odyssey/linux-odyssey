ec90c484b46737182a6f8150675f4ffada9dcfb3
wancat <wancat@wancat.cc>
2023-07-31 15:42:42 +0800

    feat: add parameters and subcommand to client.js
    - Added `"commander": "^11.0.0"` as a dependency in the `package.json` file
    - Added an `import { program } from 'commander'` statement in the `client.js` file
    - Modified the `createdSession` function in the `client.js` file to use `program.opts().host` instead of `API_ENDPOINT`
    - Added a `getSessionList` function in the `client.js` file to fetch the list of sessions
    - Added a `lastSession` function in the `client.js` file to get the last session from the session list
    - Added a `connect` function in the `client.js` file to establish a socket connection
    - Modified the `main` function in the `client.js` file to use `program.opts().session` instead of `process.argv[2]` to get the session ID
    - Modified the `main` function in the `client.js` file to use `program.opts().host` instead of `API_ENDPOINT` to initialize the socket connection
    - Added a `program` object with options and actions in the `client.js` file
    - Added `.bash_history` to the `.gitignore` file
    - Added `.viminfo` to the `.gitignore` file
    - Added `*.swp` to the `.gitignore` file
    
    Signed-off-by: wancat <wancat@wancat.cc>

--- server/src/client.js
+++ server/src/client.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import { stdin, stdout, exit } from 'process'
 import { io } from 'socket.io-client'
+import { program } from 'commander'
 import { get } from './utils/env.js'
-
-const API_ENDPOINT = get('API_ENDPOINT', 'http://localhost:3000')
 
 stdin.setRawMode(true)
 stdin.resume()
 stdin.setEncoding('utf8')
 
-async function getSessionId() {
-  const res = await fetch(`${API_ENDPOINT}/api/v1/sessions`, {
+async function createdSession() {
+  console.log('Creating a new session...')
+  const res = await fetch(`${program.opts().host}/api/v1/sessions`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -20,15 +20,37 @@ async function getSessionId() {
     }),
   })
   const data = await res.json()
-  return data._id
+  console.log('Created Session ID:', data._id)
+  return data
 }
 
-async function main() {
-  const sessionId = process.argv[2] || (await getSessionId())
+async function getSessionList() {
+  const res = await fetch(`${program.opts().host}/api/v1/sessions`)
+  const data = await res.json()
+  return data
+}
+
+async function lastSession() {
+  const sessions = await getSessionList()
+  if (sessions.length === 0) {
+    console.log(
+      'No session found. Please use --create to create a new session.'
+    )
+    exit(1)
+  }
+  const session = sessions[sessions.length - 1]
+  console.log(
+    `Last Session ID: ${session._id}, Quest: ${session.quest}, Created At: ${session.createdAt}`
+  )
+  return session
+}
+
+async function connect(sessionId) {
   console.log(`Session ID: ${sessionId}`)
+  console.log('Use Ctrl + D to exit.')
   if (!sessionId) exit()
 
-  const socket = io(API_ENDPOINT, {
+  const socket = io(program.opts().host, {
     query: {
       session_id: sessionId,
     },
@@ -57,4 +79,37 @@ async function main() {
   })
 }
 
-main()
+async function main() {
+  console.log(program.opts().session)
+  // main()
+
+  const sessionId =
+    program.opts().session ||
+    (program.opts().create ? (await createdSession())._id : null) ||
+    (await lastSession())._id
+
+  await connect(sessionId)
+}
+
+program
+  .option('-s, --session <string>', 'Session ID')
+  .option('-c, --create', 'Create a new session')
+  .option(
+    '-h, --host <string>',
+    'Server host',
+    get('API_ENDPOINT', 'http://localhost:3000')
+  )
+  .action(main)
+
+program
+  .command('list')
+  .description('List all sessions')
+  .action(async () => {
+    const sessions = await getSessionList()
+    sessions.forEach(({ _id, quest, createdAt }) => {
+      console.log(`${_id} ${quest} ${createdAt}`)
+    })
+    exit()
+  })
+
+program.parse()
