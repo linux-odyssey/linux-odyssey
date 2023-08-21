d86aa5bcc1b90902b55a5063dc070c6ef13df154
wancat <wancat@wancat.cc>
2023-08-21 20:59:35 +0800

    fix: use environment to select API_ENDPOINT

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,12 +7,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api/v1',
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: remove "/api" prefix from the path
+        target: `${process.env.API_TARGET || 'http://localhost:3000'}/api/v1`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/socket.io': {
-        target: 'ws://localhost:3000',
-        // target: 'wss://odyssey.wancat.cc',
+        target: process.env.API_TARGET || 'ws://localhost:3000',
+        changeOrigin: true,
         ws: true,
       },
     },
