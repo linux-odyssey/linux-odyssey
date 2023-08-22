cf5b761f3fce6d80de317423ffed0d8af9880f05
wancat <wancat@wancat.cc>
2023-08-22 15:49:21 +0800

    feat: collect docker scripts into one file

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-containers=$(docker ps -a | grep 'quest-' | awk '{print $1}')
-echo $containers
-
-docker rm -f $containers
--- inspect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker compose -f docker-compose.prod.yml logs backend -f --no-log-prefix
