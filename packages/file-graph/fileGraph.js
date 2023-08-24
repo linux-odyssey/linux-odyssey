ab570e9c1bcb406c970b9047f8c132d6a1d8552c
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-08-24 15:05:50 +0800

    feat: finish file-graph-algorithm (check required)

--- packages/file-graph/fileGraph.js
+++ packages/file-graph/fileGraph.js
@@ -9,18 +9,41 @@ export default class FileGraph {
     // Logic to add the file node to the graph
     // You can implement the logic to find the appropriate location in the file graph
     // If the file node already exists, throw an error
+
+    const newNode = new FileNode(file)
+    this.root.addChild(newNode, { makeParents: true })
   }
 
   remove(file) {
     // Logic to remove a file node from the graph
     // If the file node is a folder, also remove all children
     // If the file node doesn't exist, throw an error
+
+    const nodeToRemove = this.findNode(file)
+    if (nodeToRemove) {
+      const parentNode = this.findParentNode(file)
+      if (parentNode) {
+        parentNode.children = parentNode.children.filter(
+          (child) => child !== nodeToRemove
+        )
+      }
+    } else {
+      throw new Error(`File node not found: ${file.path}`)
+    }
   }
 
   discover(eventFiles) {
     // Logic to handle the discover event
     // Overwrite the file graph with the provided files
     // Remove file nodes that don't exist anymore and add new ones
+
+    const updatedNodes = eventFiles.map((file) => new FileNode(file))
+    this.root = new FileNode({
+      path: '/',
+      type: 'folder',
+      discovered: true,
+      children: updatedNodes,
+    })
   }
 
   handleEvent(event) {
