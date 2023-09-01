b41343ed689ce84da0c048587e8aeb050246ef87
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-09-01 17:50:30 +0800

    feat: add Authentication Page

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { onMounted } from 'vue'
+import socket from './utils/socket'
+import sessionManager from './utils/session'
+
 import CommandlistPart from './components/CommandlistPart.vue'
 import HeaderPart from './components/HeaderPart.vue'
 import HintPart from './components/HintPart.vue'
@@ -7,8 +10,6 @@ import QuestPart from './components/QuestPart.vue'
 import TerminalPart from './components/TerminalPart.vue'
 import VisualizationPart from './components/VisualizationPart.vue'
 import ControlPalette from './components/ControlPalette.vue'
-import socket from './utils/socket'
-import sessionManager from './utils/session'
 
 onMounted(async () => {
   await sessionManager.lastOrCreate()
@@ -36,7 +37,7 @@ onMounted(async () => {
         <section id="quest" class="h-2/3 m-3">
           <QuestPart />
         </section>
-        <hr class="border-text-line border" />
+        <hr class="border-text-tertiary border" />
         <section id="cmdlist" class="h-1/3">
           <CommandlistPart />
         </section>
---
+++ app/src/AuthenticationPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthenticationPart from './components/AuthenticationPart.vue'
+import Background from './components/DynamicBackground.vue'
+</script>
+<template>
+  <div class="relative w-screen h-screen">
+    <Background class="w-full h-full" />
+    <div
+      class="h-screen w-screen absolute top-0 left-0 flex flex-wrap justify-center content-center"
+    >
+      <div class="h-2/3 w-1/3">
+        <AuthenticationPart />
+      </div>
+    </div>
+  </div>
+</template>
