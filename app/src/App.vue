0c9d0c8b0fe593828bd981c5452f483f04efae7e
wancat <wancat@wancat.cc>
2023-09-01 15:15:12 +0800

    feat: display tasks

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,3 +1,34 @@
+<script setup>
+import { onMounted } from 'vue'
+import CommandlistPart from './components/CommandlistPart.vue'
+import HeaderPart from './components/HeaderPart.vue'
+import HintPart from './components/HintPart.vue'
+import QuestPart from './components/QuestPart.vue'
+import TerminalPart from './components/TerminalPart.vue'
+import VisualizationPart from './components/VisualizationPart.vue'
+import ControlPalette from './components/ControlPalette.vue'
+import socket from './utils/socket'
+import sessionManager from './utils/session'
+
+onMounted(async () => {
+  await sessionManager.lastOrCreate()
+  socket.connect(sessionManager.getSession())
+  socket.on('connect', function open() {
+    console.log('Connected to the server.')
+  })
+  socket.on('disconnect', function close() {
+    console.log('Disconnected from the server.')
+  })
+  socket.on('message', console.log)
+  socket.on('graph', (event) => {
+    sessionManager.handleGraphUpdate(event)
+  })
+  socket.on('tasks', (tasks) => {
+    sessionManager.setTasks(tasks)
+  })
+})
+</script>
+
 <template>
   <div class="bg-background-primary h-screen">
     <!--header-->
