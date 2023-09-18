eb65912aa1769fa9938c8f2875d44914bc8e83c4
Annie Huang <annie.huang921002@gmail.com>
2023-09-18 23:40:26 +0800

    fix format

--- app/src/components/HintPart.vue
+++ app/src/components/HintPart.vue
@@ -1,5 +1,4 @@
 <script setup>
-import FileNode from './FileNode.vue'
 import sessionManager from '../utils/session'
 </script>
 
@@ -7,10 +6,11 @@ import sessionManager from '../utils/session'
   <font-awesome-icon :icon="['far', 'lightbulb']" class="text-yellow-200 p-1" />
   <h1 class="inline text-text font-xl">Hint</h1>
   <div id="hint">
-    <li v-for="hint in sessionManager.hints.value" :key="hint.id">    
-      <p class="text-text">{{hint}}
-      <br>
-    </p>
+    <li v-for="hint in sessionManager.hints.value" :key="hint.id">
+      <p class="text-text">
+        {{ hint }}
+        <br />
+      </p>
     </li>
   </div>
 </template>
