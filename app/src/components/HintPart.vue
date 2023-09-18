fd5326ac207883ae985381c152c272b1162596c0
annie huang <annie.huang921002@gmail.com>
2023-09-18 15:58:50 +0800

    edit show method

--- app/src/components/HintPart.vue
+++ app/src/components/HintPart.vue
@@ -7,8 +7,10 @@ import sessionManager from '../utils/session'
   <font-awesome-icon :icon="['far', 'lightbulb']" class="text-yellow-200 p-1" />
   <h1 class="inline text-text font-xl">Hint</h1>
   <div id="hint">
-    <p class="text-text">--Hint--</p>
-    <p class="text-text">{{ sessionManager.hints.value }}</p>
-    <!-- <FileNode :node="sessionManager.hints.value" /> -->
+    <li v-for="hint in sessionManager.hints.value" :key="hint.id">    
+      <p class="text-text">{{hint}}
+      <br>
+    </p>
+    </li>
   </div>
 </template>
