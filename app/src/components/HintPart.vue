be8774e36a8f7c253a633adb736b26a398cd79eb
Annie Huang <annie.huang921002@gmail.com>
2023-09-19 23:01:34 +0800

    try: seperate hint content

--- app/src/components/HintPart.vue
+++ app/src/components/HintPart.vue
@@ -6,11 +6,15 @@ import sessionManager from '../utils/session'
   <font-awesome-icon :icon="['far', 'lightbulb']" class="text-yellow-200 p-1" />
   <h1 class="inline text-text font-xl">Hint</h1>
   <div id="hint">
-    <li v-for="hint in sessionManager.hints.value" :key="hint.id">
+    <div
+      class="bg-background h-full rounded-lg"
+      v-for="hint in sessionManager.hints.value"
+      :key="hint.id"
+    >
       <p class="text-text">
         {{ hint }}
-        <br />
       </p>
-    </li>
+      <br />
+    </div>
   </div>
 </template>
