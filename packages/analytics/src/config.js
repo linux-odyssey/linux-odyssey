add6bfd1003b0f2ce4d25f2c4700d61530ed1d32
wancat <wancat@wancat.cc>
2023-10-09 10:14:05 +0800

    feat: connect analytics to mongo

---
+++ packages/analytics/src/config.js
@@ -0,0 +1,3 @@
+export default {
+  db: process.env.MONGO_URL || 'mongodb://localhost:27017/odyssey-test',
+}
--- packages/analytics/src/index.js
+++ packages/analytics/src/index.js
@@ -1,17 +1,24 @@
+import connectDB from '@linux-odyssey/models'
 import express from 'express'
 import { engine } from 'express-handlebars'
+import config from './config.js'
 
-const app = express()
+async function main() {
+  await connectDB(config.db)
+  const app = express()
 
-// Setting up Handlebars as templating engine
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+  // Setting up Handlebars as templating engine
+  app.engine('handlebars', engine())
+  app.set('view engine', 'handlebars')
+  app.set('views', './views')
 
-app.get('/', (req, res) => {
-  res.render('home', { title: 'Linux Odyssey Admin' })
-})
+  app.get('/', (req, res) => {
+    res.render('home', { title: 'Linux Odyssey Admin' })
+  })
 
-app.listen(3001, () => {
-  console.log('Server started on http://localhost:3001')
-})
+  app.listen(3001, () => {
+    console.log('Server started on http://localhost:3001')
+  })
+}
+
+main().catch(console.error)
