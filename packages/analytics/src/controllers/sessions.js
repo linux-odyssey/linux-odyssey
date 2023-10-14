2257b4b8096cb0c09cb68224760538a987837591
wancat <wancat@wancat.cc>
2023-10-14 19:15:07 +0800

    feat: session detail page

--- packages/analytics/src/controllers/sessions.js
+++ packages/analytics/src/controllers/sessions.js
@@ -1,7 +1,12 @@
-import { sessionList } from '../models/sessions.js'
+import { sessionList, sessionDetail } from '../models/sessions.js'
 
-// eslint-disable-next-line import/prefer-default-export
 export async function sessionListController(req, res) {
   const sessions = await sessionList()
   res.render('sessions', { sessions })
+}
+
+export async function sessionDetailController(req, res) {
+  const { id } = req.params
+  const session = await sessionDetail(id)
+  res.render('sessionDetail', { session })
 }
--- packages/analytics/src/router.js
+++ packages/analytics/src/router.js
@@ -3,7 +3,10 @@ import { Router } from 'express'
 import { homeController } from './controllers/index.js'
 import { userListController } from './controllers/users.js'
 import { commandListController } from './controllers/commands.js'
-import { sessionListController } from './controllers/sessions.js'
+import {
+  sessionDetailController,
+  sessionListController,
+} from './controllers/sessions.js'
 
 const router = Router()
 
@@ -11,5 +14,6 @@ router.get('/', homeController)
 router.get('/users', userListController)
 router.get('/commands', commandListController)
 router.get('/sessions', sessionListController)
+router.get('/sessions/:id', sessionDetailController)
 
 export default router
