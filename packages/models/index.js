08eaa550a033e4317c0b8807ef02ec3433ebb55d
wancat <wancat@wancat.cc>
2023-10-14 15:54:56 +0800

    fix: rethrowing connectDB error

--- packages/models/index.js
+++ packages/models/index.js
@@ -6,14 +6,9 @@ export { default as Session } from './session.js'
 export { default as Command } from './command.js'
 
 export default async function connectDB(uri) {
-  try {
-    const db = await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    return db.connection.getClient()
-  } catch (err) {
-    console.error(err)
-    throw new Error('Failed to connect to MongoDB')
-  }
+  const db = await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  return db.connection.getClient()
 }
