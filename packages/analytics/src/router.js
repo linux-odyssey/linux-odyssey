9e990a2fa45452672bf0e3cbaf8afd4bd180bc5e
wancat <wancat@wancat.cc>
2023-10-14 17:10:17 +0800

    feat: users list page

--- packages/analytics/src/router.js
+++ packages/analytics/src/router.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import { homeController } from './controllers/index.js'
+import { userListController } from './controllers/users.js'
 
 const router = Router()
 
 router.get('/', homeController)
+router.get('/users', userListController)
 
 export default router
