b4a37f7606b27215520b7951174f9684fca29287
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-10-25 11:19:42 +0800

    fix: remove order in game schema

--- server/src/utils/quest.js
+++ server/src/utils/quest.js
@@ -10,35 +10,39 @@ export default async function loadAndUpdateQuests() {
   const questNames = await fs.readdir(questDirectory)
 
   const quests = Promise.all(
-    questNames.map(async (id) => {
-      // Check if id is directory
-      const stat = await fs.stat(path.join(questDirectory, id))
-      if (!stat.isDirectory()) {
-        return null
-      }
-      const fullPath = path.join(questDirectory, id, 'game.yml')
+    questNames
+      .filter((name) => !name.startsWith('.'))
+      .map(async (id) => {
+        // Check if id is directory
+        const stat = await fs.stat(path.join(questDirectory, id))
+        if (!stat.isDirectory()) {
+          return null
+        }
+        const fullPath = path.join(questDirectory, id, 'game.yml')
 
-      try {
-        // Read and parse the README.md file.
-        const body = await fs.readFile(fullPath, 'utf-8')
+        try {
+          // Read and parse the README.md file.
+          const body = await fs.readFile(fullPath, 'utf-8')
 
-        const quest = yaml.parse(body)
+          const quest = yaml.parse(body, {
+            merge: true,
+          })
 
-        return Quest.findByIdAndUpdate(
-          id,
-          {
+          return Quest.findByIdAndUpdate(
             id,
-            ...quest,
-          },
-          {
-            upsert: true,
-          }
-        )
-      } catch (error) {
-        console.error(`Error parsing quest ${id}:`, error)
-        throw error
-      }
-    })
+            {
+              id,
+              ...quest,
+            },
+            {
+              upsert: true,
+            }
+          )
+        } catch (error) {
+          console.error(`Error parsing quest ${id}:`, error)
+          throw error
+        }
+      })
   )
 
   return quests
