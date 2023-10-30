8a0512aac891b4a4d7191235834afb036b1d183a
Lin, Hung-Shin <wancat@wancat.cc>
2023-10-30 13:58:33 +0800

    feat: clear terminal on reset
    * TODO: have some trailing

--- app/src/components/ControlPalette.vue
+++ app/src/components/ControlPalette.vue
@@ -31,10 +31,12 @@
 <script setup>
 import sessionManager from '../utils/session'
 import socket from '../utils/socket'
+import socketTerminal from '../utils/terminal'
 
 const reset = async () => {
   console.log('resetting...')
   await sessionManager.createSession()
   socket.connect(sessionManager.getSession())
+  socketTerminal.clear()
 }
 </script>
