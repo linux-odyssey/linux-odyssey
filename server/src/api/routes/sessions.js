14b2c1b0d5e28479c9615335fce1f2bbc495617a
wancat <wancat@wancat.cc>
2023-10-30 23:46:39 +0800

    feat: add global and session rate limit

--- server/src/api/routes/sessions.js
+++ server/src/api/routes/sessions.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import * as sessionController from '../controllers/sessionController.js'
+import sessionRateLimit from '../../middleware/sessionRateLimit.js'
 
 const router = Router()
 
 router.get('/', sessionController.getSessionList)
-router.post('/', sessionController.createSession)
+router.post('/', sessionRateLimit, sessionController.createSession)
 router.post('/active', sessionController.getActiveSession)
 router.get('/:id', sessionController.getSessionById)
 router.delete('/:id', sessionController.deleteSessionById)
---
+++ server/src/middleware/globalRateLimit.js
@@ -0,0 +1,6 @@
+import { rateLimit } from 'express-rate-limit'
+
+export default rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 20,
+})
