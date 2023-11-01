6a64f759507ed7de2290554327597b04ca060608
Lin, Hung-Shin <wancat@wancat.cc>
2023-11-01 14:19:45 +0800

    fix: remove default user

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken'
-import { User } from '@linux-odyssey/models'
 import bcrypt from 'bcrypt'
 import config from '../config.js'
 
@@ -7,23 +6,11 @@ export function hashPassword(password) {
   return bcrypt.hash(password, 10)
 }
 
-export async function createTestUser() {
-  let user = await User.findOne({ username: 'alex' })
-  if (!user) {
-    user = new User({
-      username: 'alex',
-      email: 'alex@example.com',
-    })
-    user.hashedPassword = await hashPassword('123456')
-    await user.save()
-  }
-}
-
 export function genJWT(payload) {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
-      config.jwtSecret,
+      config.secret,
       { expiresIn: config.containerExpiry },
       (err, token) => {
         if (err) {
@@ -38,7 +25,7 @@ export function genJWT(payload) {
 
 export function verifyJWT(token) {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, config.jwtSecret, (err, decoded) => {
+    jwt.verify(token, config.secret, (err, decoded) => {
       if (err) {
         reject(err)
       } else {
