6ace3d17d1b78b55a2b0bbf5c8777804ec79d00d
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-11-03 13:57:57 +0800

    feat: add bug report button

--- app/src/components/ControlPalette.vue
+++ app/src/components/ControlPalette.vue
@@ -5,21 +5,21 @@
         :icon="['far', 'circle-question']"
         class="text-text-disabled"
       />
-      <p class="text-text-disabled inline">Solution</p>
+      <p class="text-text-disabled inline ml-1">Solution</p>
     </button>
     <button id="reset" class="bg-bg-secondary rounded-lg p-2" @click="reset">
       <font-awesome-icon
         :icon="['fas', 'arrow-rotate-left']"
         class="text-text-primary"
       />
-      <p class="text-text-primary inline">Reset</p>
+      <p class="text-text-primary inline ml-1">Reset</p>
     </button>
     <button id="continue" class="bg-bg-disabled rounded-lg p-2">
       <font-awesome-icon
         :icon="['far', 'circle-right']"
         class="text-text-disabled"
       />
-      <p class="text-text-disabled inline">Continue</p>
+      <p class="text-text-disabled inline ml-1">Continue</p>
     </button>
   </section>
 </template>
