1e728b413bafcb1fd9e07d2f24abecc54faf5788
wancat <wancat@wancat.cc>
2023-11-11 17:25:01 +0800

    fix: not reexport in default

--- server/src/api/controllers/userController.js
+++ server/src/api/controllers/userController.js
@@ -4,7 +4,6 @@ export async function me(req, res) {
   const userProfile = await UserProfile.findOne({
     user: req.user._id,
   }).populate('user')
-  console.log(userProfile)
   if (!userProfile) {
     res.status(404).json({ message: 'User not found' })
     return
@@ -18,8 +17,4 @@ export async function me(req, res) {
     email,
     progress,
   })
-}
-
-export default {
-  me,
 }
--- server/src/api/routes/userRouter.js
+++ server/src/api/routes/userRouter.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import userController from '../controllers/userController.js'
+import { me } from '../controllers/userController.js'
 import authRequired from '../../middleware/authRequired.js'
 
 const router = express.Router()
 
-router.get('/me', authRequired, userController.me)
+router.get('/me', authRequired, me)
 
 export default router
