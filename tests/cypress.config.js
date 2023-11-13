103a8dbd0f0a643c7e4cb5f149445ec5de4a8be8
Annie Huang <annie.huang921002@gmail.com>
2023-11-13 22:14:26 +0800

    update config
    (cherry picked from commit ccb19b5bbdc7d5adee05a2a77598c945783eb5b4)

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -8,5 +8,21 @@ const getConfig = (key, defaultValue) => {
 module.exports = defineConfig({
   e2e: {
     baseUrl: getConfig('BASE_URL', 'http://localhost:5173'),
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    pageLoadTimeout: 120000,
+  },
+  env: {
+    ...process.env,
+    defaultAccount: process.env.default_USERNAME,
+    defaultPassword: process.env.default_PASSWORD,
+    // google_username: process.env.GOOGLE_USERNAME,
+    // google_password: process.env.GOOGLE_PASSWORD,
+    // google_client_id: process.env.GOOGLE_CLIENT_ID,
+    // google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    // github_username: process.env.GITHUB_USERNAME,
+    // github_password: process.env.GITHUB_PASSWORD,
+    // github_client_id: process.env.GITHUB_CLIENT_ID,
+    // github_client_secret: process.env.GITHUB_CLIENT_SECRET,
   },
 })
