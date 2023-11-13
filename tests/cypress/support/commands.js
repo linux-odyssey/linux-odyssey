837712b7421b48e22aac3c359a96af4b925611dc
Annie Huang <annie.huang921002@gmail.com>
2023-11-13 22:15:39 +0800

    Update tests: - Add public command to press enter until story end - Add public command to check status pending
    (cherry picked from commit c86163fd101553d56d9280b363d18b1ef80877eb)

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -1,25 +1,57 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import '@testing-library/cypress/add-commands'
+
+Cypress.Commands.add('LoginWithPassword', (username, password) => {
+  cy.clearAllCookies({ domain: null })
+  cy.get('a.text-text-primary', { timeout: 100000 }).contains('Log in').click()
+  cy.get('#username').type(username)
+  cy.get('#password').type(password)
+  cy.findByRole('button', { name: 'Log In' }).click()
+})
+Cypress.Commands.add('PrepareForGame', () => {
+  cy.visit('/')
+  cy.LoginWithPassword(
+    Cypress.env('defaultAccount'),
+    Cypress.env('defaultPassword')
+  )
+  cy.get('.xterm-screen').as('Terminaltextbox').should('be.visible')
+  cy.findByRole('button', { name: 'Reset' }).click()
+  cy.typeInCommand('clear{enter}')
+  cy.get('@Terminaltextbox', { timeout: 150000 }).should(
+    'contain',
+    'commander:~ $'
+  )
+})
+Cypress.Commands.add('typeInCommand', (command) => {
+  cy.get('.xterm-screen', { timeout: 150000 }).type(command)
+})
+Cypress.Commands.add('getQuestInfo', (id) => {
+  return cy
+    .get('#quest')
+    .find('li')
+    .find('p')
+    .contains(`${id}`, { timeout: 100000 })
+})
+Cypress.Commands.add('checkHint', (index, total) => {
+  cy.get('#hint')
+    .find('.justify-end')
+    .contains(`${index}/${total}`)
+    .should('be.visible')
+})
+Cypress.Commands.add('waitUntilActive', () => {
+  cy.get('input[id="currentStatus"]', { timeout: 1000000 })
+    .invoke('val')
+    .then((value) => {
+      if (value === 'active' || value === 'finished') {
+        cy.log('story ends')
+        return
+      }
+      cy.typeInCommand('{enter}')
+      cy.waitUntilActive()
+    })
+})
+Cypress.Commands.add('checkPending', () => {
+  cy.get('input[id="currentStatus"]', { timeout: 1000000 }).should(
+    'have.value',
+    'pending'
+  )
+})
