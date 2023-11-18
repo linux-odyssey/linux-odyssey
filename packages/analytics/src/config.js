1ab394f1872c0b9aa0922d91d4ca1137cb939778
wancat <wancat@wancat.cc>
2023-11-18 22:22:34 +0800

    feat: add basic auth to analytics

--- packages/analytics/src/config.js
+++ packages/analytics/src/config.js
@@ -1,7 +1,16 @@
+function getOrFailed(key) {
+  if (!process.env[key]) {
+    throw new Error(`Missing required environment variable: ${key}`)
+  }
+  return process.env[key]
+}
+
 const config = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3001,
   db: process.env.MONGO_URL || 'mongodb://localhost:27017/odyssey-test',
+  username: getOrFailed('USERNAME'),
+  password: getOrFailed('PASSWORD'),
 }
 
 config.baseUrl = process.env.BASE_URL || `http://${config.host}:${config.port}`
