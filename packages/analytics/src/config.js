20280752402b1e88c4324aaa8a6cddd38f85955f
wancat <wancat@wancat.cc>
2023-11-19 14:51:36 +0800

    feat: setup admin env in prod compose

--- packages/analytics/src/config.js
+++ packages/analytics/src/config.js
@@ -9,8 +9,8 @@ const config = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3001,
   db: process.env.MONGO_URL || 'mongodb://localhost:27017/odyssey-test',
-  username: getOrFailed('USERNAME'),
-  password: getOrFailed('PASSWORD'),
+  username: getOrFailed('ADMIN_USERNAME'),
+  password: getOrFailed('ADMIN_PASSWORD'),
 }
 
 config.baseUrl = process.env.BASE_URL || `http://${config.host}:${config.port}`
