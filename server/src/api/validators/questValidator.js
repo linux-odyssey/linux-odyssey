c42c5586143aac4243c0d7eec411c010933ab981
Chen, Tai-Lin <neil.chentl@gmail.com>
2023-11-23 14:37:08 +0800

    quest get-started

--- server/src/api/validators/questValidator.js
+++ server/src/api/validators/questValidator.js
@@ -2,6 +2,6 @@ import { check } from 'express-validator'
 import { noError } from '../../middleware/validator.js'
 
 export const checkQuestId = () =>
-  check('questId').notEmpty().isString().isAlphanumeric().isLength({ max: 64 })
+  check('questId').notEmpty().isString().isLowercase().isLength({ max: 64 })
 
 export const questIdValidator = [checkQuestId(), noError]
