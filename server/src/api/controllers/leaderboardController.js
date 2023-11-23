9e7065fbd74bc036213a12105dd105d381209584
Lin, Hung-Shin <wancat@wancat.cc>
2023-11-23 22:28:36 +0800

    fix: remove fake leaderboard responses

--- server/src/api/controllers/leaderboardController.js
+++ server/src/api/controllers/leaderboardController.js
@@ -8,10 +8,5 @@ export default asyncHandler(async (req, res) => {
     completedQuests: user.completedQuests,
     score: user.score * 1000,
   }))
-  // duplicate the response 100 times
-  const fakeResponse = []
-  for (let i = 0; i < 100; i++) {
-    fakeResponse.push(...response)
-  }
-  res.json(fakeResponse)
+  res.json(response)
 })
