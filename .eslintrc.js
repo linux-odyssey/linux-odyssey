8eaec839d3b6f8b1928b0539a03d8f10fe5e8473
Lin, Hung-Shin <wancat@wancat.cc>
2023-12-14 14:32:34 +0800

    feat: add post-build-script to gradually migrate

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,32 @@ module.exports = {
     node: true,
     jest: true,
   },
-  plugins: ['cypress'],
+  plugins: ['@typescript-eslint', 'cypress'],
   extends: [
     'plugin:vue/vue3-essential',
     'airbnb-base',
     'plugin:prettier/recommended', // We added this
     'plugin:cypress/recommended',
+    // 'plugin:@typescript-eslint/recommended', // Use recommended rules from the @typescript-eslint/eslint-plugin
   ],
+  parser: '@typescript-eslint/parser', // Specify the TypeScript parser
+
   parserOptions: {
     // parser: 'babel-eslint',
     ecmaVersion: 13,
     sourceType: 'module',
   },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
     'no-console': 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'import/extensions': 'off',
     camelcase: ['error', { properties: 'never', ignoreDestructuring: true }],
     'no-param-reassign': ['error', { props: false }],
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
@@ -29,5 +39,19 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto', semi: false }],
     'import/prefer-default-export': 'off',
     'no-restricted-syntax': ['off', 'ForOfStatement'],
+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // Allow missing file extensions
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 }
