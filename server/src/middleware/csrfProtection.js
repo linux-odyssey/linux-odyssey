2ac73a06c9278e73d5c3897bad8fe52316a6cdc1
chreamy <chrislinkou@gmail.com>
2024-01-18 03:15:38 -0600

    fix: CSRF issue #51

---
+++ server/src/middleware/csrfProtection.js
@@ -0,0 +1,11 @@
+import csrf from 'csurf'
+const CSRF = csrf({ cookie: true })
+function csrfProtection(req, res, next) {
+  if (req.method === 'POST') {
+    CSRF(req, res, next)
+  } else {
+    next()
+  }
+}
+
+export default csrfProtection
