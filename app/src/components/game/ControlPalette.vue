68674db2aad80cbae2a88b95dcde7d01899f46fc
Annie Huang <annie.huang921002@gmail.com>
2024-03-05 23:54:02 +0800

    hide some text when screen is small

--- app/src/components/game/ControlPalette.vue
+++ app/src/components/game/ControlPalette.vue
@@ -20,14 +20,16 @@ const continuePlay = async () => {
 }
 </script>
 <template>
-  <section class="h-[10%] p-3 grid grid-cols-4 gap-4 place-content-between">
+  <section
+    class="h-fit w-fit px-1 py-1 grid grid-cols-4 gap-2 place-content-between"
+  >
     <button id="survey" class="bg-bg-secondary rounded-lg p-2">
       <a title="Survey" :href="surveyUrl" target="_blank" class="h-5 w-5">
         <font-awesome-icon
           :icon="['fas', 'file-invoice']"
-          class="text-text-primary"
+          class="text-text-primary px-1"
         />
-        <p class="text-text-primary inline ml-1">
+        <p class="text-text-primary inline ml-1 hidden md:flex">
           <!-- Survey -->
           問卷
         </p>
@@ -36,9 +38,9 @@ const continuePlay = async () => {
     <button disabled id="solution" class="bg-bg-disabled rounded-lg p-2">
       <font-awesome-icon
         :icon="['far', 'circle-question']"
-        class="text-text-disabled"
+        class="text-text-disabled px-1"
       />
-      <p class="text-text-disabled inline ml-1">
+      <p class="text-text-disabled inline ml-1 hidden md:flex">
         <!-- Solution -->
         解答
       </p>
@@ -46,9 +48,9 @@ const continuePlay = async () => {
     <button id="reset" class="bg-bg-secondary rounded-lg p-2" @click="reset">
       <font-awesome-icon
         :icon="['fas', 'arrow-rotate-left']"
-        class="text-text-primary"
+        class="text-text-primary px-1"
       />
-      <p class="text-text-primary inline ml-1">
+      <p class="text-text-primary inline ml-1 hidden md:flex">
         <!-- Reset -->
         重來
       </p>
@@ -60,9 +62,9 @@ const continuePlay = async () => {
     >
       <font-awesome-icon
         :icon="['far', 'circle-right']"
-        class="text-text-primary"
+        class="text-text-primary px-1"
       />
-      <p class="text-text-primary inline ml-1">
+      <p class="text-text-primary inline ml-1 hidden md:flex">
         <!-- Continue -->
         繼續
       </p>
