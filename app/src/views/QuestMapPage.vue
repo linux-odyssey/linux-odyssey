d8392ec3f64f1dbaafe62eebc3215603a2c6a4b3
Annie Huang <annie.huang921002@gmail.com>
2024-04-07 23:44:41 +0800

    delete redundant code

--- app/src/views/QuestMapPage.vue
+++ app/src/views/QuestMapPage.vue
@@ -12,14 +12,7 @@ import GameHeaderComponents from '../components/header/GameHeaderComponents.vue'
       class="z-50"
     />
   </div>
-  <!-- <p
-      class="text-text-primary absolute z-1 font-bold inline-block tracking-widest"
-      style="left: 1.2%; top: 1.5%; height: 100%; font-size: 2vh"
-    >
-      Quest Map
-      Unix編年史
-    </p> -->
-  <div class="flex flex-col justify-center items-center w-full h-full mt-8">
+  <div class="flex flex-col justify-center items-center w-full h-[94vh]">
     <QuestMap />
   </div>
 </template>
