3944e91d0cbdee1c09257a57b08e31a73ee5396e
Chen, Tai-Lin <neil.chentl@gmail.com>
2024-06-28 02:14:22 +0800

    fix: modify component name and title (HintPart=>DescriptionPart)

--- app/src/views/GamePage.vue
+++ app/src/views/GamePage.vue
@@ -5,7 +5,7 @@ import sessionStore, { init } from '../store/session'
 import HeaderPart from '../components/header/HeaderPart.vue'
 import GameHeaderComponents from '../components/header/GameHeaderComponents.vue'
 // import CommandlistPart from '../components/CommandlistPart.vue'
-import HintPart from '../components/game/HintPart.vue'
+import DescriptionPart from '../components/game/DescriptionPart.vue'
 import QuestPart from '../components/game/QuestPart.vue'
 import TerminalPart from '../components/game/TerminalPart.vue'
 import VisualizationPart from '../components/game/VisualizationPart.vue'
@@ -73,7 +73,7 @@ onMounted(async () => {
     <!-- Terminal and Hint -->
     <div class="bg-bg h-full rounded-lg w-1/2">
       <section id="hint" class="h-3/5">
-        <HintPart />
+        <DescriptionPart />
       </section>
       <hr class="border-border border" />
       <section id="terminal" class="h-2/5 overflow-hidden">
