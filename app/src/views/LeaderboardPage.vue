87df12ac85e5ff1661c09f73d8a102241bc138a1
neilchen0812 <neil.chentl@gmail.com>
2026-09-22 01:31:48 +0930

    feat(i18n): add localized content for leaderboard

--- app/src/views/LeaderboardPage.vue
+++ app/src/views/LeaderboardPage.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 import { ref, onMounted } from 'vue'
 import { useToast } from 'vue-toastification'
 import DynamicBackground from '../components/DynamicBackground.vue'
 import HeaderPart from '../components/header/HeaderPart.vue'
 import GameHeaderComponents from '../components/header/GameHeaderComponents.vue'
 import api from '../utils/api'
+
+const { t } = useI18n()
 
 const leaderboard = ref<
   {
@@ -21,7 +24,7 @@ async function getLeaderboard() {
     leaderboard.value = res.data
   } catch (err: any) {
     console.error(err)
-    toast.error('讀取排行榜失敗')
+    toast.error(t('leaderboard.load_leaderboard_failed'))
   }
 }
 
@@ -37,22 +40,33 @@ onMounted(async () => {
       class="w-screen h-screen absolute top-0 left-0 flex flex-wrap justify-center content-center"
     >
       <div class="w-full">
-        <HeaderPart title="排行榜" :headerComponent="GameHeaderComponents" />
+        <HeaderPart
+          :title="t('leaderboard.title')"
+          :headerComponent="GameHeaderComponents"
+        />
       </div>
       <div class="w-fit h-[94vh] m-3">
         <div
           class="h-full bg-bg border-8 border-bg-primary flex flex-col items-center rounded-3xl p-10"
         >
-          <h1 class="text-text-primary text-3xl mb-5">排行榜</h1>
+          <h1 class="text-text-primary text-3xl mb-5">
+            {{ t('leaderboard.title') }}
+          </h1>
           <div class="max-h-300 overflow-auto">
             <table class="w-full text-sm text-left text-text">
               <thead
                 class="text-xs text-text-secondary uppercase bg-bg-secondary"
               >
                 <tr>
-                  <th scope="col" class="px-6 py-3">使用者名稱</th>
-                  <th scope="col" class="px-6 py-3">分數</th>
-                  <th scope="col" class="px-6 py-3">完成關卡</th>
+                  <th scope="col" class="px-6 py-3">
+                    {{ t('leaderboard.username') }}
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    {{ t('leaderboard.score') }}
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    {{ t('leaderboard.completed_quests') }}
+                  </th>
                 </tr>
               </thead>
               <tbody>
